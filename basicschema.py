bb8e37df447c17b4e97c5afe54e401c5a2650700
Abhishek Mishra <geekabhi@outlook.com>
2019-11-06 15:41:41 +0530

    added schema

---
+++ basicschema.py
@@ -0,0 +1,33 @@
+import graphene
+import extraction
+import requests
+
+
+def extractdata(url):
+    html = requests.get(url).text
+    extracted = extraction.Extractor().extract(html, source_url=url)
+    return extracted
+
+
+class Website(graphene.ObjectType):
+    url = graphene.String(required=True, description="simple website url")
+    title = graphene.String()
+    description = graphene.String()
+    image = graphene.String()
+    feed = graphene.String()
+
+
+class Query(graphene.ObjectType):
+    website = graphene.Field(Website, url=graphene.String())
+
+    def resolve_website(self, info, url='https://pycon.id/'):
+        extracted = extractdata(url)
+        print(extracted)
+        return Website(url=url,
+                       title=extracted.title,
+                       description=extracted.description,
+                       image=extracted.image,
+                       feed=extracted.feed)
+
+
+schema = graphene.Schema(query=Query)
