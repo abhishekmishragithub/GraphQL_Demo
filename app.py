d7d0b296d7a6bb354394f101d72baa1ccd9a129e
Abhishek Mishra <geekabhi@outlook.com>
2019-11-06 15:43:16 +0530

    Added app

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask_graphql import GraphQLView
+from basicschema import schema
+
+app = Flask(__name__)
+app.add_url_rule(
+  '/graphql',
+  view_func=GraphQLView.as_view('graphql', schema=schema, graphiql=True)
+)
+app.run()
